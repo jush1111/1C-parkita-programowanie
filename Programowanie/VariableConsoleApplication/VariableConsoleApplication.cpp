911416e29a6c93473561bde31bde2dd9fa118328
fil <nhhj>
2023-10-09 16:03:05 +0200

    KONIECC C LEKCJE

--- Programowanie/VariableConsoleApplication/VariableConsoleApplication.cpp
+++ Programowanie/VariableConsoleApplication/VariableConsoleApplication.cpp
@@ -17,37 +17,68 @@
 void task1()
 {
 
-    int numberFromUser;
-    std::cout << "Podaj liczbe\n";
-    std::cin >> numberFromUser;
+	int numberFromUser;
+	std::cout << "Podaj liczbe\n";
+	std::cin >> numberFromUser;
 
-    std::cout << "Podales " << numberFromUser << "\n";
+	std::cout << "Podales " << numberFromUser << "\n";
 }
 
 //Program obliczaj¹cy œredni¹ arytmetyczn¹ dwóch liczb.
 void task2()
 {
-    int firstNumber, secondNumber;
-    std::cout << "Podaj pierwsz¹ liczbê\n";
-    std::cin >> firstNumber;
-    std::cout << "Podaj drug¹ liczbê\n";
-    std::cin >> secondNumber;
+	int firstNumber, secondNumber;
+	std::cout << "Podaj pierwsz¹ liczbê\n";
+	std::cin >> firstNumber;
+	std::cout << "Podaj drug¹ liczbê\n";
+	std::cin >> secondNumber;
 
-    int average;
-    average = (firstNumber + secondNumber) / 2;
+	int average;
+	average = (firstNumber + secondNumber) / 2;
 
-    std::cout << "Œrednia to: " << average << "\n";
+	std::cout << "Œrednia to: " << average << "\n";
+}
+//Program obliczaj¹cy pole prostok¹ta
 void task3()
+{
+	int first_side, second_side;
+	std::cout << "podaj pierwszy bok\n";
+	std::cin >> first_side;
+	std::cout << "podaj drugi bok\n";
+	int secondnumber;
+	std::cin >> second_side;
+	int area;
+	area = first_side * second_side;
+	std::cout << "pole wynosi:";
+	std::cout << area;
+}
+//Program obliczaj¹cy pole sto¿ka 
+void task4()
+{
+	int pi, radius, l;
+	std::cout << "podaj promien\n";
+	std::cin >> radius;
+	std::cout << "podaj l\n";
+	std::cin >> l;
+	
+
+
+
 
 }
 
 
 
+
+
+
+
 int main()
 {
-    setlocale(LC_CTYPE, "polish");
-    //task1();
-    //task2();
+	setlocale(LC_CTYPE, "polish");
+	//task1();
+	//task2();
+	//task3(); 
 }
 
 /*
@@ -60,41 +91,41 @@ algorytm- skoñczony zbiór instrukcji, który rozwi¹zuje zadany problem. Okreœla t
 Zapis algorytmu:
 * rysunki
 * opis s³owny
-* w punktach 
-* schemat blokowy 
-* kod Ÿród³owy danego jêzyka programowania 
-* pseudokod 
+* w punktach
+* schemat blokowy
+* kod Ÿród³owy danego jêzyka programowania
+* pseudokod
 
 Zmienna- pewien obszar w pamieci operacyjnej, w której mo¿na w danej chwili przechowaæ tylko jedn¹ dan¹.
 
 Instrukcja deklaracji zmiennej:
-typ_zmiennej nazwa_zmiennej; 
+typ_zmiennej nazwa_zmiennej;
 
-typ zmiennej- wielkoœæ obszaru pamiêci, interpretacja ci¹gu bitów 
+typ zmiennej- wielkoœæ obszaru pamiêci, interpretacja ci¹gu bitów
 int - liczba ca³kowita ze znakiem <-2 147 483 648, 2 147 483 647>
 short - 2 bajtowa liczba ca³kowita ze znakiem <-32 768, 32 767>
-long - to samo co int 
+long - to samo co int
 long long - 8 bajtowa liczba ze znakiem <-9 223 372 036 854 775 808, 9 223 372 036 854 775 807>
 
 unsigned - zmienna bez znaku <0, 2*max + 1>
 
-float - 4 bajtowa liczba rzeczywista, dok³adnoœæ 6-7 cyfr po przecinku 
-double - 8 bajtowa liczba rzeczywista, dok³adnoœæ 15-16 cyfr po przecinku 
-long double - 12 bajtowa liczba rzeczywista, dok³adnoœæ 19-20 cyfr po przecinku 
+float - 4 bajtowa liczba rzeczywista, dok³adnoœæ 6-7 cyfr po przecinku
+double - 8 bajtowa liczba rzeczywista, dok³adnoœæ 15-16 cyfr po przecinku
+long double - 12 bajtowa liczba rzeczywista, dok³adnoœæ 19-20 cyfr po przecinku
 
-Nazwa zmiennej - nazwa obszaru pamiêci, identyfikator 
+Nazwa zmiennej - nazwa obszaru pamiêci, identyfikator
 Warunki niezbêdne:
 * dozwolone znaki:
-    -alfabet angielski aA-zZ
-    -cyfry arabskie 0-9 
-    -podkreœlenie (pod³oga) _
-* pierwszym znakiem nie mo¿e byæ cyfra 
+	-alfabet angielski aA-zZ
+	-cyfry arabskie 0-9
+	-podkreœlenie (pod³oga) _
+* pierwszym znakiem nie mo¿e byæ cyfra
 * unikalny w swoim zakresie widocznoœci
-* nie mo¿e to byæ s³owo kluczowe (zarezerwowane) danego jêzyka 
+* nie mo¿e to byæ s³owo kluczowe (zarezerwowane) danego jêzyka
 
 Warunki programistów:
-* nazwa zmiennej powinna oddawaæ charakter przechowywanych danych 
+* nazwa zmiennej powinna oddawaæ charakter przechowywanych danych
 * je¿eli nazwa sk³ada siê z wielu wyrazów w miejscu spacij podkreœlenie lub zaczynaj¹c od durgiego s³owa piszemy je
 z du¿ej litery
-* piszemy po angielsku 
+* piszemy po angielsku
 */
