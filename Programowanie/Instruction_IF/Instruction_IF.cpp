0e2be0f69d910600a2068cc5c0de1c3b16b63ed9
fil <nhhj>
2023-10-16 16:01:22 +0200

    gff

--- Programowanie/Instruction_IF/Instruction_IF.cpp
+++ Programowanie/Instruction_IF/Instruction_IF.cpp
@@ -30,8 +30,30 @@ void task1()
 
 	std::cout << "kolejna instrukcja\n";
 }
+//Napisz program, który wyœwietli informacje czy liczba jest parzysta czy nieparzysta.
+void task2()
+{
+	int number;
+	std::cout << "Podaj liczbe:\n";
+	std::cin >> number;
+
+	int rest = number % 2; 
+/*
+	if (rest == 0)
+		std::cout << "Liczba jest parzysta\n";
+
+	if (rest != 0) // rest == 1
+		std::cout << "liczba jest nieparzysta\n";
+*/
+
+	if (rest == 0)
+		std::cout << "Liczba jest parzysta\n";
+	else
+		std::cout << "liczba jest nieparzysta\n";
+}
 
 int main()
 {
-	task1();
+	//task1();
+	//task2();
 }
