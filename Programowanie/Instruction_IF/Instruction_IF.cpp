3017ccec7c47f51f84f36dd32422e7fc96b383ad
fil <nhhj>
2023-10-17 09:52:11 +0200

    koniec lekcji cos zrobilem ale nie duzo hihihi

--- Programowanie/Instruction_IF/Instruction_IF.cpp
+++ Programowanie/Instruction_IF/Instruction_IF.cpp
@@ -9,6 +9,19 @@ Operatory warunkowe:
 == - równe 
 != - ró¿ne 
 
+Operatory logiczne:
+&& - AND 
+|| - OR 
+! - NOT 
+
+a	b	a&&b	a||b	!a
+F	F	  F		  F		 T
+F	T	  F		  T		 F
+T	F	  F		  T		 F
+T	T	  T		  T		 F
+
+F - false
+T - true 
 */
 
 
@@ -50,10 +63,115 @@ void task2()
 		std::cout << "Liczba jest parzysta\n";
 	else
 		std::cout << "liczba jest nieparzysta\n";
+} 
+
+//napisz program, który wyœwitli informacje czy liczba jest z zakresu <1 ; 10).
+void task3()
+{
+	int number;
+	std::cout << "Podaj liczbe:\n";
+	std::cin >> number;
+
+	//wersja 1
+	if (number >= 1) //NUMBER > 0
+	{
+		if (number < 10) //NUMBER <= 9
+			std::cout << "liczba jest w przedziale\n";
+		else
+			std::cout << "liczba z poza zakresu\n";
+	}
+	else
+		std::cout << "liczba z poza zakresu\n";
+
+	//wersja 2
+	if (number >= 1) 
+		if (number < 10)
+			std::cout << "Liczba jest w przedziale\n";
+		else
+			std::cout << "liczba z poza zakres\n";
+	else 
+		std::cout << "liczba z poza zakresu\n";
+
+	//wersja 3
+	if (number < 1 || number >= 10)
+		std::cout << "liczba z poza zakresu";
+	else
+		std::cout << "Liczba jest w przedziale\n";
+
+	//wersja 4
+	if (!(number >= 1 && number < 10))
+			std::cout << "liczba z poza zakresu";
+	else
+			std::cout << "Liczba jest w przedziale\n";
+
 }
+//napisz program, który wykona dzielenia dwóch liczb
+void task4()
+{
+	int firstNumber, secondNumber;
+	std::cout << "podaj liczbe:\n";
+	std::cin >> firstNumber;
+	std::cout << "podaj liczbe:\n";
+	std::cin >> secondNumber; 
+
+	if (secondNumber != 0)
+	{
+		int quoitent = firstNumber / secondNumber;
+		std::cout << "wynik dzielenia " << quoitent << "\n";
+	}
+	else
+		std::cout << "dzielenie przez zero!!!\n"; 
+}
+ 
+
+/*
+*. Napisz program, który poprosi u¿ytkownika o podanie liczby od 1 do 7 i wyœwietli odpowiadaj¹cy mu dzieñ tygodnia.
+*. Napisz program, który poprosi u¿ytkownika o podanie dwóch liczb ca³kowitych i wyœwietli wiêksz¹ z nich.
+*. Napisz program, który poprosi u¿ytkownika o podanie roku i sprawdzi, czy jest to rok przestêpny. Wyœwietl odpowiedni komunikat.
+*. Napisz program, który poprosi u¿ytkownika o podanie liczby ca³kowitej i sprawdzi, czy jest ona podzielna zarówno przez 3, jak i przez 5. Wyœwietl odpowiedni komunikat.
+*. Napisz program, który poprosi u¿ytkownika o podanie masy cia³a (w kilogramach) i wzrostu (w metrach).
+Na podstawie tych danych oblicz wskaŸnik BMI (Body Mass Index) i wyœwietl odpowiedni komunikat informuj¹cy o stanie zdrowia.
+*. Napisz program, który poprosi u¿ytkownika o podanie d³ugoœci trzech odcinków i sprawdzi, czy mo¿na zbudowaæ z nich trójk¹t. Wyœwietl odpowiedni komunikat.
+*. Napisz program, który poprosi u¿ytkownika o podanie liczby dodatniej i obliczy jej pierwiastek kwadratowy, jeœli jest to mo¿liwe. Wyœwietl wynik lub odpowiedni komunikat, jeœli liczba jest ujemna.
+*. Program sprawdzaj¹cy czy podana data jest poprawna (np. sprawdzaj¹c, czy dzieñ jest z zakresu od 1 do 31, miesi¹c od 1 do 12 itd.)
+*/
+
+//Napisz program, który poprosi u¿ytkownika o podanie dwóch liczb ca³kowitych i sprawdzi, czy s¹ one równe.Wyœwietl odpowiedni komunikat.
+void task5()
+{
+	int number1, number2;
+	std::cout << "podaj pierwsza liczbe:\n";
+	std::cin >> number1;
+	std::cout << "podaj durga liczbe:\n";
+	std::cin >> number2;
+
+	if (number1 == number2)
+		std::cout << "te liczby sa równe\n";
+	else
+		std::cout << "te liczby nie sa równe\n";
+}
+//Napisz program, który poprosi u¿ytkownika o podanie wieku i sprawdzi, czy osoba jest pe³noletnia. Wyœwietl odpowiedni komunikat.
+void task6()
+{
+	int age;
+	std::cout << "podaj swoj wiek\n";
+	std::cin >> age; 
+
+	if (age >= 18)
+		std::cout << "ta osoba jest pelnoletnia\n";
+	else
+		std::cout << "wiek to tylko liczba";
+}
+// Napisz program, który poprosi u¿ytkownika o podanie liczby ca³kowitej i obliczy jej wartoœæ bezwzglêdn¹. Wyœwietl wynik.
+void task7()
 
 int main()
 {
+	setlocale(LC_CTYPE, "polish");
 	//task1();
 	//task2();
+	//task3();
+	//task4();
+	//task5();
+	task6();
 }
