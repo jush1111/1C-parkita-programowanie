8491f7ea8ac079b0509ae91025d08774eaf36796
fil <nhhj>
2023-11-03 14:22:04 +0100

    italodisco i sanfrancisco pica

--- Programowanie/ConsoleApplication/ConsoleApplication.cpp
+++ Programowanie/ConsoleApplication/ConsoleApplication.cpp
@@ -34,11 +34,28 @@ void task1()
 
 void task2()
 {
+	int numberFromUser;
+
+	do
+	{
+		std::cout << "podaj liczbe dodatnia:\n";
+		std::cin >> numberFromUser;
+	} while (numberFromUser < 0);
+
+	std::cout << "liczba dodatnia pobrana od uzytkownika" << numberFromUser << "\n";
+
+}
+
+//napisz program ktory wylosuje liczbe a nastepnie uzytkownik bedzie musial ja zgadnac.
+void task3()
+{
 
 }
 
 
 int main()
 {
-	task1();
+	//task1();
+	//task2();
+	task3();
 }
