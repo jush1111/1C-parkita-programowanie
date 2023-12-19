54385eb0c1cdd1250877657b75eef97098ccdc3e
fil <nhhj>
2023-12-19 09:52:07 +0100

    koniec lekcji

--- Programowanie/CharactersConsoleApplication/CharactersConsoleApplication.cpp
+++ Programowanie/CharactersConsoleApplication/CharactersConsoleApplication.cpp
@@ -34,13 +34,125 @@ void task2()
 	*/
 }
 
+//Napisz program, ktory poprosi cie o twoje imie i cie przywita
+void task3()
+{
+	std::string userName;
+	std::cout << "podaj swoje imie\n";
+	std::cin >> userName;
+
+	std::cout << "witaj " << userName << "tutaj \n";
+}
+
+//Napisz program sprawdzaj¹cy czy podane haslo jest poprawne
+//(np jeslihaslo jest "abc123" program powinien wyswietlic "haslo poprawne"
+//jesli jest inne powinien wyswietlic "haslo niepoprawne")
+void task4()
+{
+	std::string password;
+	std::cout << "podaj haslo\n";
+	std::cin >> password;
+
+	if (password == "abc123")
+		std::cout << "haslo poprawne\n";
+	else
+		std::cout << "haslo niepoprawne\n";
+}
+
+//napisz program ktory wczyta lancuch znakow i policzy ile jest malych liter 'a'
+void task5()
+{
+	std::string textFromUser;
+	std::cout << "podaj lancuhc nzsakow \n";
+	std::cin >> textFromUser;
+
+	int numberOfCharacters = 0;
+
+	//std::cout << "pierwszy znak to " << textFromUser[0] << "\n";
+	//textFromUser[1] = 'x';
+	//std::cout << "ilosc znakow " <<  textFromUser.length() << "\n";
+
+	for (int i = 0; i < textFromUser.length(); i++)
+	{
+		if (textFromUser[i] == 'a')
+			numberOfCharacters++;
+	}
+	std::cout << "malych liter 'a' jest " << numberOfCharacters << "\n";
+}
 
 
+/*
+* Program sprawdzaj¹cy czy podany ci¹g znaków jest palindromem (czyli takim, który czytany od ty³u jest taki sam, jak czytany od przodu, np. "kajak")
+* Program sprawdzaj¹cy czy podane dwa s³owa s¹ anagramami (czyli takimi, które zawieraj¹ te same litery, ale w innym uk³adzie, np. "klasa" i "salka")
+* Program wyci¹gaj¹cy informacje z numeru PESEL
+* Program implementuj¹cy algorytm szyfrowania Cezara (proste szyfrowanie, w którym ka¿dy znak w tekœcie jest zastêpowany innym znakiem, przesuniêtym o sta³¹ liczbê pozycji w alfabecie).
+* Program który na wejœciu przyjmie równanie a na wyjœciu da równanie w odwrotnej notacji polskiej ONP. Np. na wejœciu 2+3*4 na wyjœcu da 234*+
+* Program, który na wejœciu przyjmie rówanie w ONP a na wyjœciu wyœwietli wynik rówania.
+* Algorytm szyfrowania AtBash Cipher - algorytm szyfruj¹cy, w którym ka¿da litera jest zamieniana na literê z przeciwnej strony alfabetu (np. A na Z, B na Y itd.)
+*/
+
+// Napisz program, który bêdzie prosi³ o has³o. Nie przepuœci dalej dopóki nie zostanie ono podane prawid³owo.
+void task6()
+{
+	std::string password;
+		do
+		{
+			std::cout << "podaj haslo\n";
+			std::cin >> password;
+			
+		}while (password != "abc123");
+		std::cout << "gratulacje dobre chaslo";
+}
+
+//Napisz program, który pobiera od u¿ytkownika ci¹g znaków i wyœwietla liczbê samog³osek i spó³g³osek w tym ci¹gu.
+void task7()
+{
+	std::string charactersFromUser;
+	std::cout << "podaj ciag znakow";
+	std::cin >> charactersFromUser;
+
+	int numberOfVowel = 0; //samog³oska
+	int numberOfConsonant = 0; //spó³g³oska
+
+	for (int i = 0; i < charactersFromUser.length(); i++)
+	{
+		if (charactersFromUser[i] == 'a'
+			|| charactersFromUser[i] == '¹'
+			|| charactersFromUser[i] == 'e'
+			|| charactersFromUser[i] == 'ê'
+			|| charactersFromUser[i] == 'i'
+			|| charactersFromUser[i] == 'o'
+			|| charactersFromUser[i] == 'i'
+			|| charactersFromUser[i] == 'y')
+			numberOfVowel++;
+		else if (charactersFromUser[i] >= 'a' && charactersFromUser[i] <= 'z')
+			numberOfConsonant++;
+	}
+	std::cout << "w tym ciagu jest " << numberOfVowel << " samogloski                                                                                                                                                                                                                                                                                                      i " << numberOfConsonant << " spo³g³oski \n";
+}
+
+//Poproœ u¿ytkownika o wprowadzenie liczby ca³kowitej w systemie dziesiêtnym.
+//Nastêpnie skonwertuj tê liczbê na system dwójkowy (binarny) i wyœwietl wynik.
+void task8()
+{
+	std::string decimalNumber;
+	std::cout << "podaj liczbe calkowita w systemie dziesietnym\n";
+	std::cin >> decimalNumber;
+	
+	
+}
 
 int main()
 {
 	setlocale(LC_CTYPE, "polish");
-	task1();
+	//task1();
+	//task2();
+	//task3()
+	//task4()
+	//task5();
+	//task6();
+	//task7();
+	task8();
 }
 
 /*
