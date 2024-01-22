8b7471dfa37b418f086b8808f9bc736d3d07eee5
fil <nhhj>
2024-01-22 16:01:38 +0100

    jdsad uSDADSAD LDSADSA K ASD

--- Programowanie/ArrayConsoleApplication/ArrayConsoleApplication.cpp
+++ Programowanie/ArrayConsoleApplication/ArrayConsoleApplication.cpp
@@ -86,9 +86,12 @@ void task3()
 	std::cout << "min to:" << min  << "\n";
 }
 
+
+//Napisz program, który wyznaczy wszystkie liczby pierwsze od 2 do zadeklarowanego zakresu. Metoda sito Eratostenesa.
 void task4()
 {
-	const long long UPPER_RANGE = 70;
+	const long long UPPER_RANGE = 1000000;
+
 	//wersja 1
 
 	for (long long numberToCheck = 2; numberToCheck <= UPPER_RANGE; numberToCheck++)
@@ -102,20 +105,64 @@ void task4()
 				break;
 			}
 		}
-		if (isPrime /*== true*/)
-			std::cout << numberToCheck << ",";
+
+		if (isPrime)
+			std::cout << numberToCheck << ", ";
 	}
-	std::cout << "gotowe\n";
+	std::cout << "\n";
 
+	//wersja 2
+	bool sieveOfEratosthenes[UPPER_RANGE + 1];
 
+	for (unsigned long long i = 2; i <= UPPER_RANGE; i++)
+	{
+		sieveOfEratosthenes[i] = true;
+	}
 
+	for (unsigned long long number = 2; number <= UPPER_RANGE; number++)
+	{
+		if (sieveOfEratosthenes[number] /*== true*/)
+		{
+			for (long long numberToCrossOut = number + number; numberToCrossOut <= UPPER_RANGE; numberToCrossOut = numberToCrossOut + number)
+				sieveOfEratosthenes[numberToCrossOut] = false;
+		}
+	}
 
+	for (unsigned long long i = 2; i <= UPPER_RANGE; i++)
+	{
+		if (sieveOfEratosthenes[i] /*== true*/)
+			std::cout << i << ", ";
+	}
+	std::cout << "\n";
 }
+
+
+//Napisz program, który wczyta numer dnia tygodnia a nastepnie wyswietli nazee tego dnia lub komunikat bledu
+void task5()
+{
+	int numberOfWeek;
+	std::cout << "podaj numer dnia tygodnia\n";
+	std::cin >> numberOfWeek;
+
+	std::string dayNames[] = { "Poniedziałek", "Wtorek", "środa", "czwartek", "piatek", "sobota", "niedziela" };
+	//dayNames[0] = "poniedzialek";
+	//dayNames[1] ="wtorek';
+	//kontynukacja dni
+
+	if (numberOfWeek >= 0 && numberOfWeek <= 6)
+		std::cout << "ten dzien to" << dayNames[numberOfWeek] << "\n";
+	else
+		std::cout << "niepoprawany dzien\n";
+}
+
+
 
 int main()
 {
 	setlocale(LC_CTYPE, "polish");
 	//task1();
 	//task2();
-	task3();
+	//task3();
+	//task4();
+	task5();
 }
