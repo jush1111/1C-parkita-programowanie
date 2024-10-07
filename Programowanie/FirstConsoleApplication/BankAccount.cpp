03dd7b994b38a73725723ea21a28dde8be8c9b5b
q <q>
2024-10-07 09:38:49 +0200

    fdsfsdfd

---
+++ Programowanie/FirstConsoleApplication/BankAccount.cpp
@@ -0,0 +1,13 @@
+#pragma once
+
+class BankAccount;
+{
+public:
+	double balance;
+	std::string owner;
+	std::string currency;
+
+	void AccountInformation
+		void DepositToAccount(double ammunt);
+	bool WitdthdrawalDromAccount(double amount);
+};
---
+++ Programowanie/FirstConsoleApplication/BankAccount.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+#include <iostream>
+class BankAccount
+{
+public:
+	double balance;
+	std::string owner;
+	std::string currency;
+
+	void AccountInfo()
+	{
+		std::cout << "Informacje o koncie bankowym\n";
+		std::cout << "W³aœciciel: " << owner << "\n";
+		std::cout << "Saldo: " << balance << " " << currency << "\n";
+	}
+
+	void DepositToAccount(double amount)
+	{
+		if (amount >= 0)
+			balance = balance + amount;
+	}
+
+	bool WithdrawToAccount(double amount)
+	{
+		if (amount <= 0 && balance >= amount)
+		{
+			balance = balance - amount;
+			return true;
+		}
+		return false;
+	}
+};
