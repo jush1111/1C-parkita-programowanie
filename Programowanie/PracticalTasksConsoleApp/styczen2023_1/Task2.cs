2197ae2989f4be53aa995d05132fdefb316d23c3
Sala213 <Sala213@ezse.pl>
2025-10-16 16:50:20 +0200

    dsadsa

---
+++ Programowanie/PracticalTasksConsoleApp/styczen2023_1/Task2.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticalTasksConsoleApp.styczen2023_1
+{
+    internal class Task2
+    {
+
+        public void task()
+        {
+
+            note n1 = new note("shopping", "buy milk");
+            note n2 = new note("meeting", "at 10:00AM");
+
+            n1.display();
+            n2.display();
+
+            n1.checking();
+            n2.checking();
+        }
+    }
+
+}
+
+
+class note
+{
+    private static int counterNotes = 0;
+
+    private int id;
+
+    protected string title;
+    protected string contents;
+
+    public note(string t, string c)
+    {
+        counterNotes++;
+        id = counterNotes;
+        title = t;
+        contents = c;
+    }
+    public void display()
+    {
+
+        Console.WriteLine($"Tytuł: {title}");
+        Console.WriteLine($"Treść: {contents}");
+    }
+
+    public void checking()
+    {
+        Console.WriteLine($"ID: {id}; Licznnik: {counterNotes}; Tytuł: {title}; Treść: {contents}");
+    }
+}
